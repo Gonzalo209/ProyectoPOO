b10671c512177d4c3f122eb4401e70ffc548b2bf
Gonzalo209 <cgonzalo983@gmail.com>
2023-10-05 13:31:34 -0600

    Subiendo proyecto

---
+++ Main.java
@@ -0,0 +1,11 @@
+import vista.VistaLogin;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        new VistaLogin();
+
+    }
+    
+}
---
+++ vista/VistaLogin.java
@@ -0,0 +1,21 @@
+package vista;
+
+import javax.swing.*;
+
+public class VistaLogin extends JFrame {
+
+    public VistaLogin() {
+
+        super("Login");
+
+        setVisible(true);
+
+        setSize(600, 300);
+
+        setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+}
